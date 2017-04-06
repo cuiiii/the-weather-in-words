b1422853829d336daa2b7ad4401ba050f81debc9
clhuang <cuilyn88@gmail.com>
2017-04-05 20:33:07 -0400

    more design changes, still in working state w/o geolocation

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,5 @@
+// using jquery plugin "simpleweather"
+
 // browser support
 if ("geolocation" in navigator) {
   $('.js-geolocation').show(); 
@@ -14,8 +16,21 @@ $('.js-geolocation').on('click', function() {
 
 
 $(document).ready(function() {
-  loadWeather('','1062617'); //input location and/or woeid
+  loadWeather('','1062617'); //input location and/or woeid, default is Singapore
 });
+
+function printWeather(code) {
+  $.simpleweather({
+    code: code,
+    success: function(weather) {
+       if(weather.code >= 0 && weather.code <=2) {
+        html += ("<p>Stay the fuck inside. Unless you're a storm chaser.</p>");
+      } else if (weather.code >= 3 && weather.code <=4) {
+        html += ("<p>You're going to need some waterproofing. In case the thunder didn't already tell you.</p>");
+      } 
+    }
+  })
+}
 
 function loadWeather(location, woeid) {
   $.simpleWeather({
@@ -23,10 +38,14 @@ function loadWeather(location, woeid) {
     woeid: woeid,
     unit: 'f',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html = '<h2><i class="icon-'+weather.code+'"></i></h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      html += '<li>'+weather.temp+'&deg;F / '+weather.alt.temp+'&deg;C</li></ul>'; 
+
+      //print current weather code (for testing purposes)
+      html += '<p>'+weather.code+'</p>'
+      // html += '<p>'+printWeather(weather.code+)'</p>'
       
       $("#weather").html(html);
     },
